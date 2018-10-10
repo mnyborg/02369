b5cafaa0e6655d448045b93ffb34b7c7df4005d5
mnyborg <manyb@dtu.dk>
2018-10-10 15:10:40 +0200

    initial commit

---
+++ mediator/src/ConcreteColleague.java
@@ -0,0 +1,8 @@
+public class ConcreteColleague extends Colleague {
+    public ConcreteColleague(Mediator m) {
+        super(m);
+    }
+    public void receive(String message) {
+        System.out.println("Colleague Received: " + message);
+    }
+}
