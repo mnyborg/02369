3ba6af3806c26ca8e745dedf0ed72af40fa361b0
mnyborg <manyb@dtu.dk>
2020-08-19 12:48:03 +0200

    20200819

---
+++ di/src/EmailService.java
@@ -0,0 +1,7 @@
+public class EmailService implements MessageService {
+    public boolean sendMessage(String msg, String receipient) {
+        //some fancy code to send email
+        System.out.println("Email Message sent to "+receipient+" with message="+msg);
+        return true;
+    }
+}
---
+++ di/src/MessageService.java
@@ -0,0 +1,3 @@
+public interface MessageService {
+    boolean sendMessage(String msg, String receipient);
+}
